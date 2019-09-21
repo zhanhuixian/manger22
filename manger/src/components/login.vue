6cc54e4b557835cde0b5c5b53615718480e9db86
zhanhuixian <zhanhuixian@qq.com>
2019-09-21 15:56:31 +0800

    登录页面完成

---
+++ manger/src/components/login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="content">
+    <div class="form_box">
+      <el-form :label-position="labelPosition" :rules="rules" ref="ruleForm" label-width="80px">
+        <h2>用户登录</h2>
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="ruleForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="ruleForm.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      labelPosition: "top",
+      ruleForm: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 11个字符", trigger: "blur" }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.content {
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .form_box {
+    width: 580px;
+    height: 437px;
+    background-color: #fff;
+    border-radius: 4px;
+    box-sizing: border-box;
+    padding: 46px;
+    h2 {
+      margin-bottom: 24px;
+    }
+  }
+}
+</style>
--- manger/src/main.js
+++ manger/src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 Vue.config.productionTip = false
+// 全局导入基础样式
+import "./assets/css/base.less"
+// element相关
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+// 路由相关
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+import login from "./components/login.vue"
+const routes=[{
+  path:"/login",component:login
+}]
+const router=new VueRouter({
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
